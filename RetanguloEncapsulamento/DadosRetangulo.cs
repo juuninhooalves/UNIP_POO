3f91f09e07fa46673d733383192b6b7000e9e414
Edilson Junior <edilson.junior@adiq.com.br>
2020-03-01 18:39:12 -0300

    Retangulo Encapsulamento

---
+++ RetanguloEncapsulamento/DadosRetangulo.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetanguloEncapsulamento
+{
+    class DadosRetangulo
+    {
+        private double ladoA;
+        private double ladoB;
+        private double perimetro;
+        private double area;
+
+        public DadosRetangulo()
+        {
+            this.ladoA = 0;
+            this.ladoB = 0;
+            this.perimetro = 0;
+            this.area = 0;
+        }
+
+        public DadosRetangulo(double ladoA, double ladoB)
+        {
+            this.ladoA = 0;
+            this.ladoB = 0;
+            this.perimetro = 0;
+            this.area = 0;
+        }
+
+        public void setLadoA(double ladoA)
+        {
+            this.ladoA = ladoA;
+        }
+        public double getLadoA()
+        {
+            return ladoA;
+        }
+        public void setLadoB(double ladoB)
+        {
+            this.ladoB = ladoB;
+        }
+        public double getLadoB()
+        {
+            return ladoB;
+        }
+
+        public double getPerimetro()
+        {
+            return perimetro;
+        }
+
+        public double getArea()
+        {
+            return area;
+        }
+        
+        public void calcularPerimetro()
+        {
+            perimetro = ladoA * 2 + ladoB * 2;
+        }
+        public void calcularArea()
+        {
+            area = ladoA * ladoB;
+        }
+    }
+}
---
+++ RetanguloEncapsulamento/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetanguloEncapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DadosRetangulo Retangulo = new DadosRetangulo();
+
+            //Entrada de dados
+            Console.WriteLine("Retangulo: Calcula perimetro e area");
+            Console.WriteLine();
+
+            Console.WriteLine("Digite o lado A: ");
+            Retangulo.setLadoA(Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine("Digite o lado B: ");
+            Retangulo.setLadoB(Convert.ToDouble(Console.ReadLine()));
+
+            // processamento
+
+           Retangulo.calcularPerimetro();
+            Retangulo.calcularArea();
+
+
+            Console.WriteLine();
+            Console.WriteLine("  ===== RETANGULO ============");
+            Console.WriteLine("Lado A    = " + Retangulo.getLadoA());
+            Console.WriteLine("Lado B    = " + Retangulo.getLadoB());
+            Console.WriteLine("Perimetro = " + Retangulo.getPerimetro());
+            Console.WriteLine("Area      = " + Retangulo.getArea());
+
+            Console.WriteLine("Tecle <Enter> para continuar: ");
+            Console.ReadKey();
+        }
+    }
+}
