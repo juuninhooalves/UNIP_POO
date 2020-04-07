d3c0de810eee8510585fbaa90734dbb0f55ef821
Edilson Junior <edilson.junior@adiq.com.br>
2020-04-07 01:14:23 -0300

    Att

--- MediaEncapsulamento/Notas.cs
+++ MediaEncapsulamento/Notas.cs
@@ -8,89 +8,46 @@ namespace MediaEncapsulamento
 {
     class Notas
     {
-        public double NP1 { get; set; }
-        public double NP2 { get; set; }
-        public double PIM { get; set; }
-        public double MeP { get; set; }
+        private double np1;
+        private double np2;
+        private double pim;
+        private double media;
 
         public Notas()
         {
-            NP1 = 0;
-            NP2 = 0;
-            PIM = 0;
-            MeP = 0;
+            np1 = 0;
+            np2 = 0;
+            pim = 0;
+            media = 0;
         }
+
+        public double NP1
+        {
+            get { return np1; }
+            set { this.np1 = value; }
+        }
+
+        public double NP2
+        {
+            get { return np2; }
+            set { this.np2 = value; }
+        }
+
+        public double PIM
+        {
+            get { return pim; }
+            set { this.pim = value; }
+        }
+
+        public double Media
+        {
+            get { return media; }
+        }
+
 
         public void CalculaMedia()
         {
-            MeP = (NP1 * 0.4) + (NP2 * 0.4) + (PIM * 0.2); 
+            media = (NP1 * 0.4) + (NP2 * 0.4) + (PIM * 0.2); 
         }
-
-
-
-
-        #region Metodo Professor
-        //private double np1;
-        //private double np2;
-        //private double pim;
-        //private double mp;
-
-        //public Notas()
-        //{
-        //    this.np1 = 0;
-        //    this.np2 = 0;
-        //    this.pim = 0;
-        //    this.mp = 0;
-        //}
-
-        //public double NP1 { get; set; }
-
-        //public double NP2 { get; set; }
-        //public double PIM { get; set; }
-        //public double MP { get; set; }
-
-
-        //
-        //public double getNP1()
-        //{
-        //    return np1;
-        //}
-
-        //public void setNP1(double np1)
-        //{
-        //    this.np1 = np1 * 0.4;
-        //}
-        //public double getNP2()
-        //{
-        //    return np2;
-        //}
-
-        //public void setNP2(double np2)
-        //{
-        //    this.np2 = np2 * 0.4;
-        //}
-
-        //public double getPIM()
-        //{
-        //    return pim;
-        //}
-
-        //public void setPIM(double pim)
-        //{
-        //    this.pim = pim * 0.2;
-        //}
-
-        //public void CalculaMedia()
-
-
-        //{
-        //    this.mp = np1 + np2 + pim;
-        //}
-
-        //public double getMP()
-        //{
-        //    return mp;
-        //}
-        #endregion
     }
 }
--- MediaEncapsulamento/Program.cs
+++ MediaEncapsulamento/Program.cs
@@ -32,48 +32,13 @@ namespace MediaEncapsulamento
             Console.WriteLine("NP1                : " + notas.NP1);
             Console.WriteLine("NP2                : " + notas.NP2);
             Console.WriteLine("PIM                : " + notas.PIM);
-            Console.WriteLine("Média Final        : " + notas.MeP);
+            Console.WriteLine("Média Final        : " + notas.Media);
             Console.WriteLine();
             Console.WriteLine("==========================================");
             Console.WriteLine();
 
             Console.WriteLine("Tecle<ENTER> para continuar: ");
             Console.ReadKey();
-
-
-            #region Metodo Professor 
-
-            //Notas notas = new Notas();
-
-            //Console.WriteLine("Média Ponderada - UNIP");
-            //Console.WriteLine();
-
-            //Console.Write("Digite a nota da sua NP1: ");
-            //notas.setNP1(Convert.ToDouble(Console.ReadLine()));
-
-            //Console.Write("Digite a nota da sua NP2: ");
-            //notas.setNP2(Convert.ToDouble(Console.ReadLine()));
-
-            //Console.Write("Digite a nota da sua PIM: ");
-            //notas.setPIM(Convert.ToDouble(Console.ReadLine()));
-
-            //notas.CalculaMedia();
-
-            //Console.WriteLine();
-            //Console.WriteLine("=========== Resultado da Média ==========");
-            //Console.WriteLine();
-            //Console.WriteLine("NP1                : " + notas.getNP1());
-            //Console.WriteLine("NP2                : " + notas.getNP2());
-            //Console.WriteLine("PIM                : " + notas.getPIM());
-            //Console.WriteLine("Média Final        : " + notas.getMP());
-            //Console.WriteLine();
-            //Console.WriteLine("==========================================");
-            //Console.WriteLine();
-
-            //Console.WriteLine("Tecle<ENTER> para continuar: ");
-            //Console.ReadKey();
-
-            #endregion
         }
     }
 }
